e4d1d4d1b65db6075792677f436166eff6970a87
yzx <yzx24133@163.com>
2019-03-14 14:36:45 +0800

    修改上拉

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,13 +3,17 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    imgUrls: ["../images/banner.gif","https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640",  "https://images.unsplash.com/photo-1551446591-142875a901a1?w=640"],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 300
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+ jump:function(){
+   wx.navigateTo({
+     url: '../index/index',
+   })
+ }
+
 })
